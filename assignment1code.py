a1a85549716582f3fef64e2e07eaadc0aa171189
Behzad Fuladpanjeh Hojaghan <behzad.fuladpanjehho@ucalgary.ca>
2025-05-11 23:06:20 -0600

    add python code

---
+++ assignment1code.py
@@ -0,0 +1,9 @@
+# ENSF 692 Spring 2025
+# Assignment 1 Starter Code
+# Behzad Fuladpanjeh hojaghan!
+
+studentName = "Behzad Fuladpanjeh hojaghan"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+print("I specialize in blending chemical engineering expertise with machine learning and data science.")
